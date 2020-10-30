96f9bace73d767f197c4636c809345c697b2593e
Igor Wysocki <narghar@gmail.com>
2020-10-30 09:34:57 +0100

    add: 1 day solution

---
+++ 30-days-of-code/01-data-types.rb
@@ -0,0 +1,14 @@
+# Declare second integer, double, and String variables.
+int = gets.to_i
+double = gets.to_f
+str = gets.to_s
+
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+puts i + int
+# Print the sum of the double variables on a new line.
+puts d + double
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+puts s + str
